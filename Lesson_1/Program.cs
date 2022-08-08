d7b941da7f3fc9d725708c3932b0b829979b09f9
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-08 10:45:25 +0700

    Lesson1

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,1 +1,79 @@
-﻿
+﻿//  First programm
+
+// Семинар 1
+
+// 1. Напишите программу, которая будет выводить число, а потом возводить его в квадрат
+
+Console.Clear();
+
+// Console.Write("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number;
+// // number *= number
+// // int sqrt1 = Convert.ToInt32(Math.Pow(number,2));
+
+// Console.Write("Число в квадрате: " + sqrt);
+
+// 2. Напишите программу, которая на вход принимает два числа и проверяет, явлется ли первое число квадратом второго
+
+// Console.Write("Input first number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int square = num2 * num2;
+
+// if (num1 == square)
+//     Console.Write("First number is a square of a second nuber");
+
+// else
+//      Console.Write("First number is not a square of a second nuber");
+
+//3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.Write("Input number of the day ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+{
+    Console.Write("Incorrect number ");
+}
+
+else
+{
+    if (day == 1)
+    {
+        Console.Write("Monday");
+    }
+
+    if (day == 2)
+    {
+        Console.Write("Tuesday");
+    }
+
+    if (day == 3)
+    {
+        Console.Write("Wensday");
+    }
+
+    if (day == 4)
+    {
+        Console.Write("Thursday");
+    }
+
+    if (day == 5)
+    {
+        Console.Write("Friday");
+    }
+
+    if (day == 6)
+    {
+        Console.Write("Saturday");
+    }
+
+    if (day == 7)
+    {
+        Console.Write("Sunday");
+    }
+}
+
+
