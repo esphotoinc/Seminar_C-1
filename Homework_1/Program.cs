071e1f049beef5d5aae7ece97919392b25c8965b
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-08 16:55:31 +0700

    Homework_1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,73 @@
+﻿Console.Clear();
+
+// 1. Напишите программу, которая на вход принимает 2 числа и выдает, какое число больше, какое меньше.
+
+// Console.Write("Input first number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if  (num1 > num2)
+//     Console.Write("First number - max");
+
+// if (num1 < num2)
+//     Console.Write("Second number - max");
+
+// else Console.Write("First number = Second number");
+
+// 2. Напишите программу, которая на вход принимает 3 числа и выдает максимальное из этих чисел. 
+
+// Console.Write("Input first number ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input third number ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+
+// {
+//     if (num1 > num3)
+//         Console.Write("First number = max ");
+// }
+
+// {
+//     if (num1 < num3)
+//         Console.Write("Third number = max ");
+// }
+// }
+
+// else
+// {
+// {
+//     if (num2 > num3)
+//         Console.Write("Second number = max ");
+// }
+
+// {
+//     if (num2 < num3)
+//         Console.Write("Third number = max ");
+// }
+// }
+
+// 3. Напишите программу, котора на вход принимает число и выдает, является ли оно четным (делится на 0 без остатка)
+
+// Console.Write("Input number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if  (number % 2 == 0)
+//     Console.Write("Even numbered");
+
+// else Console.Write("Odd numbered");
+
+// 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+// Console.Write("Input number ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// for (int x = 2; x < num; x++)
+// if (x % 2 == 0)
+// Console.WriteLine (x);
+
+
