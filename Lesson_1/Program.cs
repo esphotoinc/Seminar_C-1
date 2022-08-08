d6f153e2a4ede8144301daa25914138aafb55b0b
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-08 16:28:11 +0700

    Lesson1

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -30,50 +30,50 @@ Console.Clear();
 
 //3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
 
-Console.Write("Input number of the day ");
-int day = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number of the day ");
+// int day = Convert.ToInt32(Console.ReadLine());
 
-if (day < 1 || day > 7)
-{
-    Console.Write("Incorrect number ");
-}
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Incorrect number ");
+// }
 
-else
-{
-    if (day == 1)
-    {
-        Console.Write("Monday");
-    }
+// else
+// {
+//     if (day == 1)
+//     {
+//         Console.Write("Monday");
+//     }
 
-    if (day == 2)
-    {
-        Console.Write("Tuesday");
-    }
+//     if (day == 2)
+//     {
+//         Console.Write("Tuesday");
+//     }
 
-    if (day == 3)
-    {
-        Console.Write("Wensday");
-    }
+//     if (day == 3)
+//     {
+//         Console.Write("Wensday");
+//     }
 
-    if (day == 4)
-    {
-        Console.Write("Thursday");
-    }
+//     if (day == 4)
+//     {
+//         Console.Write("Thursday");
+//     }
 
-    if (day == 5)
-    {
-        Console.Write("Friday");
-    }
+//     if (day == 5)
+//     {
+//         Console.Write("Friday");
+//     }
 
-    if (day == 6)
-    {
-        Console.Write("Saturday");
-    }
+//     if (day == 6)
+//     {
+//         Console.Write("Saturday");
+//     }
 
-    if (day == 7)
-    {
-        Console.Write("Sunday");
-    }
-}
+//     if (day == 7)
+//     {
+//         Console.Write("Sunday");
+//     }
+// }
 
 
