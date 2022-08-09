f86338c2ea750822ac6d75fb7f4f7a3f8bb3454a
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-10 00:16:58 +0700

    Homework 2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int num = new Random().Next(10,1000);
+// System.Console.WriteLine(num);
+// int first = num / 10;
+// int second = first % 10;
+// System.Console.WriteLine (second);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+// if (numText.Length > 2)
+// {
+//     Console.WriteLine("третья цифра  " + numText[2]);
+// }
+// else
+// {
+//     Console.WriteLine(" третьей цифры нет");
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+// Console.Write("Введите день недели ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Не верно введен день");
+// }
+// else if (day <= 2 || day <= 5)
+// {
+//     Console.Write("Не является выходным");
+// }
+// else
+// {
+//     Console.Write("Ура, сегодня выходной!");
+// }
