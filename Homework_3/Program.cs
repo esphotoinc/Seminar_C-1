0dcf4ce516d0dd9a818f56856cfa0bae585fb725
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-16 02:04:35 +0700

    Homework_3_ready

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -52,45 +52,20 @@
 
 // // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-// // 1 вариант.
 
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-int cycle = 1;
-int cycleCube = Convert.ToInt32(Math.Pow(cycle, 3));
-int max = Convert.ToInt32(Math.Pow(num, 3));
 
-while (cycleCube < max)
-{
-    {
-        Console.WriteLine($"cycle ={cycle}");
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// int cycle = 1;
 
-    }
+// while (cycle <= num)
+// {
+//     {
+//         Console.WriteLine(Math.Pow(cycle,3));
 
-    cycle = cycle++;
+//     }
+
+//     cycle++;
     
-}
-
-// // 2 вариант.
-
-// void Cube(int[] cube){
-//   int counter = 0;
-//   int length = cube.Length;
-//   while (counter <  length){
-//     cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-//     counter++;
-//   }
 // }
 
-// void PrintArry(int[] coll){
-//   int count = coll.Length;
-//   int index = 0;
-//   while(index < count){
-//     Console.Write(coll[index]+ " ");
-//     index++;
-//   }
-// } 
-
-// int[] arry = new int[cube+1];
-// Cube(arry);
-// PrintArry(arry);
