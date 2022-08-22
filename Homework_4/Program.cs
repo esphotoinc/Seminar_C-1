9590de33fe917fcdc64401458672b8538a539243
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-22 20:15:21 +0700

    Homework_4 ready

---
+++ Homework_4/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.Write("Введите число ");
+// int num = int.Parse(Console.ReadLine());
+// Console.Write ("Введите степень ");
+// int n = int.Parse(Console.ReadLine());
+// int num_n = 1;
+//   for(int i = 0; i < n; i++) 
+//   {
+//    num_n *= num;
+//   }
+//   Console.WriteLine("{0} * {1} = {2}", num, n, num_n);
+//   Console.ReadKey();
+//   return 0;
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+// // Заполнение массива
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index++;
+//     }
+
+// }
+
+// // Печать массива
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.WriteLine(col[position]);
+//         position++;
+//     }
+
+// }
+
+// // Вызов массива
+
+// int[] array = new int[8]; 
+
+// FillArray(array);
+// PrintArray(array);
+
+
+
+
