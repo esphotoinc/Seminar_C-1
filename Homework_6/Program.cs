3571a5216de6134e4d7d7ad9136f9ed8a8b433e8
Nikiforova Elena <esphotoinc@gmail.com>
2022-08-27 17:46:41 +0700

    Homework_6 ready

---
+++ Homework_6/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Console.Write("Введите элементы через пробел : ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+// Console.Write("введите первую координату: ");
+// int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите вторую координату: ");
+// int k1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите третью координату: ");
+// int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите четвертую координату: ");
+// int k2 = Convert.ToInt32(Console.ReadLine());
+// int x = 0;
+// int y = 0;
+
+// if (k1 == k2)
+// {
+//     Console.WriteLine("Прямые парралельны");
+// }
+// else if ((k1 == k2) && (b1 == b2))
+// {
+//     Console.WriteLine("Прямые совпадают друг с дургом");
+// }
+
+// else
+// {
+//     x = (b2 - b1) / (k2 - k1);
+//     y = k1 * ((b2 - b1) / (k2 - k1)) + b1;
+//     Console.WriteLine($"Координаты точки пересечения{x}, {y}");
+// }
+
+
+
