2635763530c9d316183959f023f5412f3c7bf02d
Nikiforova Elena <esphotoinc@gmail.com>
2022-09-04 01:15:33 +0700

    3d massive and other

---
+++ Homework_8/Task_58/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.WriteLine($"\nВведите размер матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите количество строк 1ой матрицы: ");
+int n = InputNumbers("Введите число стобцов 1ой и строк 2ой матрицы: ");
+int p = InputNumbers("Введите количество строк 2ой матрицы: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,]firstMatrix = new int [m,n];
+CreateArray(firstMatrix);
+WriteArray(firstMatrix);
+
+int[,]secondMatrix = new int [n,p];
+CreateArray(secondMatrix);
+WriteArray(secondMatrix);
+
+int[,]resultMatrix = new int [m,p];
+MultyMatrix (firstMatrix, secondMatrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матрицы");
+WriteArray(resultMatrix);
+
+void MultyMatrix(int[,] firstMatrix, int[,] secondMatrix, int[,] resultMatrix)
+{ 
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+         int sum = 0;
+         for (int k = 0; k < firstMatrix.GetLength(1); k++)
+         {
+            sum += firstMatrix[i,k]*secondMatrix[k,j];
+         }   
+         resultMatrix[i,j] = sum; 
+        }
+    }
+
+
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void WriteArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine ();
+  }
+}
---
+++ Homework_8/Task_62/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int n = 4;
+int [,] sqrmatrix = new int[n,n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqrmatrix.GetLength(0) * sqrmatrix.GetLength(1))
+{
+  sqrmatrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < sqrmatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqrmatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqrmatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqrmatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
